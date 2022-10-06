1b9a21b0864d7b1f915b595763213321498fb600
Abhinav Gupta <abhinavgupta0907@gmail.com>
2022-10-06 16:35:05 -0700

    quick sort

---
+++ quicksort.py
@@ -0,0 +1,24 @@
+def quicksort(arr):
+    qs(arr, 0, len(arr) - 1)
+
+def qs(arr, l, r):
+    if l >= r:
+        return
+    p = partition(arr, l, r)
+
+    qs(arr, l, p - 1)
+    qs(arr, p + 1, r)
+
+def partition(arr, l, r):
+    pivot = arr[r]
+    i = l - 1
+    for j in range(l, r):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[r] = arr[r], arr[i + 1]
+    return i + 1
+
+ll = [1000,21,4,78,221,431,67,12,1,7]
+quicksort(ll)
+print(ll)
